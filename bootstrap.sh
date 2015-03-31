415aa9c6a03e294a50a6ec04e8fe31a53fbb6a04
Jacob Cook <jacob@peakwinter.net>
2015-03-30 20:56:02 -0400

    add itsdangerous; refresh modules on pull

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@ curl "https://www.archlinux.org/mirrorlist/?country=CA&protocol=http&ip_version=
 #pacman --noconfirm -Syu
 
 # Install system dependencies
-pacman --noconfirm -Sy pkg-config gcc iptables python2 python2-pip redis arkos-redis openldap arkos-openldap python2-nginx python2-pacman python2-ntplib python2-passlib python2-pyopenssl python2-iptables python2-nginx python2-dbus python2-cryptsetup python2-pyparted python2-ldap python2-psutil python2-netifaces python2-gitpython python2-gnupg python2-flask python2-redis python2-pillow mysql-python nginx git postfix dovecot mariadb nodejs supervisor php php-fpm php-xcache php-tidy php-gd php-intl php-ldap ruby uwsgi uwsgi-plugin-python2 php-sqlite python2-pgpdump python2-lxml spambayes
+pacman --noconfirm -Sy pkg-config gcc iptables python2 python2-pip redis arkos-redis openldap arkos-openldap python2-nginx python2-pacman python2-ntplib python2-passlib python2-pyopenssl python2-iptables python2-dbus python2-cryptsetup python2-pyparted python2-ldap python2-psutil python2-netifaces python2-gitpython python2-gnupg python2-flask python2-redis python2-pillow mysql-python nginx git postfix dovecot mariadb nodejs supervisor php php-fpm php-xcache php-tidy php-gd php-intl php-ldap ruby uwsgi uwsgi-plugin-python2 php-sqlite python2-pgpdump python2-lxml python2-itsdangerous spambayes
 
 # Prepare arkOS working environment
 mkdir -p /var/lib/arkos
